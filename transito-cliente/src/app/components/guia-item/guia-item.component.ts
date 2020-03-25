db8c655762073faa90af649604781efe17195bfe
jorge <jegco13@hotmail.com>
2020-03-25 00:23:30 -0500

    css arreglado, error de autenticacion solucionado

--- transito-cliente/src/app/components/guia-item/guia-item.component.ts
+++ transito-cliente/src/app/components/guia-item/guia-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { GuiaDeTramite } from 'src/app/models/GuiaDeTramite';
 import { BaseComponent } from 'src/app/pages/base/base.component';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-guia-item',
@@ -12,13 +13,17 @@ export class GuiaItemComponent implements OnInit {
   @Input() guia: GuiaDeTramite;
   @Output() details = new EventEmitter<GuiaDeTramite>();
 
-  constructor() { }
+  constructor(private readonly sanitizer: DomSanitizer) { }
 
   ngOnInit() {
   }
 
   showDetails(): void {
     this.details.emit(this.guia);
+  }
+
+  descripcionComoHTML(descripcion: string) {
+    return this.sanitizer.bypassSecurityTrustHtml(descripcion);
   }
 
 }
